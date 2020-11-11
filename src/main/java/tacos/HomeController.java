eb9ef937d9fcf6a05dd16558d954db3be22b37c5
Rasika <rasika.narayanan@disneystreaming.com>
2020-11-11 12:15:44 -0800

    Entire Ch 1 and 2

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") // handles the requests for the root path
+    public String home() {
+        return "home"; // returns the view name
+    }
+}
